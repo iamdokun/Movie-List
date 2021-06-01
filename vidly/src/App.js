35b22d26d69c4a7b3dcfbc8ccf08946bd58de8c5
iamdokun <dokun.abraham@gmail.com>
2021-06-01 04:49:01 +0100

    I Created a movies list

---
+++ vidly/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Movies from './components/Movies';
+
+
+
+
+function App() {
+  return (
+  <Movies />
+  //  <main className='container'>
+  //    <h1>Hello World</h1>
+  //  </main>
+  );
+}
+
+export default App;
